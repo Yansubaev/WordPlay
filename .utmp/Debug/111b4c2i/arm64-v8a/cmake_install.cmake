60fccd117504b9553845d7f2d6a0039500874605
Yansubaev <yansubd@gmail.com>
2025-06-02 00:51:34 +0500

    setting target framerate, first build

--- .utmp/Debug/111b4c2i/arm64-v8a/cmake_install.cmake
+++ .utmp/Debug/111b4c2i/arm64-v8a/cmake_install.cmake
@@ -45,7 +45,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Development/Unity/WordPlay/.utmp/Debug/111b4c2i/arm64-v8a/GameActivity/cmake_install.cmake")
-  include("C:/Development/Unity/WordPlay/.utmp/Debug/111b4c2i/arm64-v8a/FramePacing/cmake_install.cmake")
 
 endif()
 
